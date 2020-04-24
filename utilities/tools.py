2da19bb3859dc0eb2f42b373d3a8621473fe9249
Miguel <miguel_corona@hotmail.com>
2020-04-24 16:28:10 -0500

    Update tools.py

--- utilities/tools.py
+++ utilities/tools.py
@@ -11,33 +11,23 @@ def timestamp():
     try:
         stamp = str(f'{now.tm_year}.{now.tm_mon}.{now.tm_mday}'
                         f'_{now.tm_hour}.{now.tm_min}.{now.tm_sec}')
-
-    except Exception as val:
-        print("Something went wrong!")
-        print("Error: ", val)
-
     return stamp
 
 
 def aciFaultDisect(infile):
     """This function expects an ACI fault file in JSON format and will return a list of tuples with the
     (fault_code, fault_severity, fault_description"""
-    code_list = []
     try:
-        with infile:
-            json_data = json.load(infile)
-            imdata_list = (json_data.get("imdata"))
-            for imdata in imdata_list:
-                attrib_dict = imdata.get("faultInst")
-                code_dict = attrib_dict.get("attributes")
-                code_str = (code_dict["code"])
-                sev_str = (code_dict["severity"])
-                descr_str = (code_dict["descr"])
-                code_list.append(tuple([code_str, sev_str, descr_str]))
+        with open(infile, "r") as file:
+            data = json.load(file)
+        imdata = data["imdata"]
+        item_key = list(imdata[0].keys())
+        attributes = [item[item_key[0]]["attributes"] for item in imdata]
+        code_list = [tuple([item["code"], item["severity"], item["descr"]]) for item in attributes]
         return code_list
 
     except Exception as error:
-        print("Error reading file: ", error)
+        print("Error reading JSON file: ", error)
 
 
 def sortTuple(tup):
@@ -49,4 +39,4 @@ def sortTuple(tup):
         return tup
 
     except Exception as error:
-        print(error)
+        print("Error sorting tuple: ", error)
